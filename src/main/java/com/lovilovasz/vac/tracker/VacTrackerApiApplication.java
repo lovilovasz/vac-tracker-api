70a3a739743ee7ef829c33c56d1d1eeb74da1c3e
Kristof Lovasz <lkristof65@gmail.com>
2024-07-22 20:01:53 +0200

    spring init

---
+++ src/main/java/com/lovilovasz/vac/tracker/VacTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.lovilovasz.vac.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacTrackerApiApplication.class, args);
+	}
+
+}
